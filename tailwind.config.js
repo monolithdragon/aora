b5bdcbf124834d4e9aba12935f15c402d413a61f
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-11-10 15:08:42 +0100

    Update with colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,37 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#161622',
+        secondary: {
+          DEFAULT: '#FF9C01',
+          100: '#FF9001',
+          200: '#FF8E01',
+        },
+        black: {
+          DEFAULT: '#000000',
+          100: '#1E1E2D',
+          200: '#232533',
+        },
+        gray: {
+          100: '#CDCDE0',
+        },
+      },
+      fontFamily: {
+        poppins: {
+          light: ['Poppins-Light', 'sans-serif'],
+          'extra-light': ['Poppins-ExtraLight', 'sans-serif'],
+          thin: ['Poppins-Thin', 'sans-serif'],
+          regular: ['Poppins-Regular', 'sans-serif'],
+          medium: ['Poppins-Medium', 'sans-serif'],
+          semiBold: ['Poppins-SemiBold', 'sans-serif'],
+          bold: ['Poppins-Bold', 'sans-serif'],
+          'extra-bold': ['Poppins-ExtraBold', 'sans-serif'],
+          black: ['Poppins-Black', 'sans-serif'],
+        },
+      },
+    },
   },
   plugins: [],
 };
