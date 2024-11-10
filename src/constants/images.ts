198f6b0722491c54ba703c609b8a24f9048d46c0
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-11-10 18:16:23 +0100

    Create images

---
+++ src/constants/images.ts
@@ -0,0 +1,9 @@
+import Logo from '../../assets/images/logo.png';
+import Cards from '../../assets/images/cards.png';
+import Empty from '../../assets/images/empty.png';
+import SmallLogo from '../../assets/images/logo-small.png';
+import Path from '../../assets/images/path.png';
+import Profile from '../../assets/images/profile.png';
+import Thumbnail from '../../assets/images/thumbnail.png';
+
+export { Logo, Cards, Empty, SmallLogo, Path, Profile, Thumbnail };
