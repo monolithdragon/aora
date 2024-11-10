1d4e7141d9a6192a3153ef802882b7c15e4ffa28
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-11-10 15:10:14 +0100

    Create icons

---
+++ src/constants/icons.ts
@@ -0,0 +1,27 @@
+const bookmark = require('../../assets/icons/bookmark.png');
+const home = require('../../assets/icons/home.png');
+const plus = require('../../assets/icons/plus.png');
+const profile = require('../../assets/icons/profile.png');
+const leftArrow = require('../../assets/icons/leftArrow.png');
+const menu = require('../../assets/icons/menu.png');
+const search = require('../../assets/icons/search.png');
+const upload = require('../../assets/icons/upload.png');
+const logout = require('../../assets/icons/logout.png');
+const eye = require('../../assets/icons/eye.png');
+const eyeHide = require('../../assets/icons/eyeHide.png');
+const play = require('../../assets/icons/play.png');
+
+export default {
+  bookmark,
+  home,
+  plus,
+  profile,
+  leftArrow,
+  menu,
+  search,
+  upload,
+  logout,
+  eye,
+  eyeHide,
+  play,
+};
