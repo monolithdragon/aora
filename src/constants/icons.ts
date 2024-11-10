59d85181835435b79f97f12f5ebebc88fa785ea4
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2024-11-10 18:16:44 +0100

    Fix import

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -1,27 +1,27 @@
-const bookmark = require('../../assets/icons/bookmark.png');
-const home = require('../../assets/icons/home.png');
-const plus = require('../../assets/icons/plus.png');
-const profile = require('../../assets/icons/profile.png');
-const leftArrow = require('../../assets/icons/left-arrow.png');
-const menu = require('../../assets/icons/menu.png');
-const search = require('../../assets/icons/search.png');
-const upload = require('../../assets/icons/upload.png');
-const logout = require('../../assets/icons/logout.png');
-const eye = require('../../assets/icons/eye.png');
-const eyeHide = require('../../assets/icons/eye-hide.png');
-const play = require('../../assets/icons/play.png');
+import Bookmark from '../../assets/icons/bookmark.png';
+import Home from '../../assets/icons/home.png';
+import Plus from '../../assets/icons/plus.png';
+import Profile from '../../assets/icons/profile.png';
+import LeftArrow from '../../assets/icons/left-arrow.png';
+import Menu from '../../assets/icons/menu.png';
+import Search from '../../assets/icons/search.png';
+import Upload from '../../assets/icons/upload.png';
+import Logout from '../../assets/icons/logout.png';
+import Eye from '../../assets/icons/eye.png';
+import EyeHide from '../../assets/icons/eye-hide.png';
+import Play from '../../assets/icons/play.png';
 
 export {
-  bookmark,
-  home,
-  plus,
-  profile,
-  leftArrow,
-  menu,
-  search,
-  upload,
-  logout,
-  eye,
-  eyeHide,
-  play,
+  Bookmark,
+  Home,
+  Plus,
+  Profile,
+  LeftArrow,
+  Menu,
+  Search,
+  Upload,
+  Logout,
+  Eye,
+  EyeHide,
+  Play,
 };
